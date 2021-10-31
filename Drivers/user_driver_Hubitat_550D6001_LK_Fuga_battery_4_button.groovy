c6c3345ea0887560c3b65d0d56f927b4b66c93a5
Stephan Heinrichsen <akafester@me.com>
2021-10-31 19:16:50 +0100

    Update user_driver_Hubitat_550D6001_LK_Fuga_battery_4_button.groovy
    removed license stuff.

--- Drivers/user_driver_Hubitat_550D6001_LK_Fuga_battery_4_button.groovy
+++ Drivers/user_driver_Hubitat_550D6001_LK_Fuga_battery_4_button.groovy
@@ -1,22 +1,6 @@
 /**
  *  Lauritz Knudsen 4-knap batteri tryk Fuga
  *
- *  Copyright 2017 Akafester
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- * AnotherUser modified from code written by motley74 and sticks18.
- * Original source: https://github.com/motley74/SmartThingsPublic/blob/master/devicetypes/motley74/osram-lightify-dimming-switch.src/osram-lightify-dimming-switch.groovy
- * This is modified on the bases made by AnotherUser
- * https://github.com/AnotherUser17/SmartThingsPublic-1/blob/master/devicetypes/AnotherUser/osram-lightify-4x-switch.src/osram-lightify-4x-switch.groovy
- *
  * v19072021 - Uploaded to Github. Initial release
  */
 
